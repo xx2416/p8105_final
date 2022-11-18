9fb7457ddb318ce7c3244ead649113a23b753c84
Tianshu Liu <tl3196@columbia.edu>
2022-11-18 17:46:37 -0500

    get and save yelp data

--- data_pre/inspection_pre.R
+++ data_pre/inspection_pre.R
@@ -1,15 +1,15 @@
 ###################################
 # import and clean inspection data
 # data source: https://data.cityofnewyork.us/Health/DOHMH-New-York-City-Restaurant-Inspection-Results/43nn-pn8j
-# data last update: November 15, 2022
-# version: 1.0
+# data last update: November 17, 2022
+# version: 2.0
 # author: Sugar
 ###################################
 
 library(tidyverse)
 
 inspection_df = 
-  read_csv("./data/raw/NYC_Restaurant_Inspection_Results.csv") %>% 
+  read_csv("./data/raw/DOHMH_New_York_City_Restaurant_Inspection_Results.csv") %>% 
   janitor::clean_names() %>% 
   drop_na(dba) %>% 
   mutate(
@@ -18,14 +18,40 @@ inspection_df =
     record_date = as.Date(record_date, "%m/%d/%Y")
   ) %>% 
   mutate(
+    boro = na_if(boro, "0"),
+    building = na_if(building, "N/A"),
+    street = na_if(street, "N/A")
+  ) %>% 
+  mutate(
     boro = fct(boro),
     action = fct(action),
     critical_flag = fct(critical_flag),
     grade = fct(grade),
     inspection_type = fct(inspection_type)
-  )
+  ) %>% 
+  arrange(desc(inspection_date))
+
+rest_df = 
+  inspection_df %>% 
+  select(camis, dba, boro, building, street, zipcode) %>% 
+  distinct() %>% 
+  mutate(
+    across(everything(), as.character)
+  ) %>% 
+  rowid_to_column()
 
 # export
 inspection_df %>% write_csv(file = "./data/inspection_data.csv")
+rest_df %>% write_csv(file = "./data/restaurant_data.csv")
 
 # read_csv("./data/inspection_data.csv") %>% View()
+
+###################################
+# # Install Git Large file system
+#
+# $ brew install git-lfs
+# $ git lfs install
+# $ git lfs track "*.csv"
+# $ git add .gitattributes
+# 
+# Add, Commit, Push
--- data_pre/yelp_pre.R
+++ data_pre/yelp_pre.R
@@ -1,98 +1,234 @@
 ###################################
 # extract review data from Yelp*
-# data last update: November 16, 2022
-# version: 1.0
+# data last update: November 17, 2022
+# version: 2.0
 # author: Sugar
 ###################################
 
 library(tidyverse)
 library(rvest)
 library(httr)
+library(urltools)
 
 # read restaurant info from inspection data set
 res_data = 
-  read_csv("./data/inspection_data.csv") %>% 
-  select(camis, dba, boro, building, street, zipcode) %>% 
+  read_csv("./data/restaurant_data.csv") %>% 
   mutate(
     across(everything(), as.character)
-  ) %>% 
-  distinct()
+  ) 
 
 # check missing values
-skimr::skim_without_charts(res_data)
+# skimr::skim_without_charts(res_data)
 
 # generate yelp url
 res_data = 
   res_data %>% 
-  replace_na(list(building = " ", zipcode = " "))%>% 
+  replace_na(list(boro = " ", building = " ", street = " ", zipcode = " "))%>% 
   mutate(
-    url = str_c("https://www.yelp.com/search?find_desc=", dba, "&find_loc=", building,"+", street, "+",boro,"+", zipcode) %>% 
-      str_replace_all(" ", "+")
+    url = str_c("https://www.yelp.com/search?find_desc=", url_encode(dba), 
+                "&find_loc=", url_encode(building),"+", 
+                url_encode(street), "+", 
+                url_encode(boro),"+", zipcode) %>% 
+      str_replace_all("%20", "+")
   )
 
 # function of getting review data
 get_info = function(url){
-  html_page = read_html(url)
- 
-   data = 
-    html_page %>% 
-    html_elements(".list__09f24__ynIEd") %>% 
-    html_text() %>% .[1]
-  
-  info = 
-    html_page %>% 
-    html_elements(".css-chan6m") %>% 
-    html_text() 
-  
-  review_num = 
-    info[2]
-  
-  distance = 
-    info[4] %>% 
-    str_split("Miles", n=2) %>% 
-    .[[1]] %>% .[1] %>% 
-    as.double()
-  
-  name = 
-    html_page %>%  
-    html_elements(".css-1egxyvc") %>% 
-    html_text() %>% .[1] %>% 
-    str_sub(4)
-  
-  price =
-    html_page %>% 
-    html_elements(".css-1s7bx9e") %>%
-    html_text() %>% .[1]
+  tryCatch({
+    html_page = read_html(url)
     
-   # type = 
-   #   html_page %>% 
-   #   html_elements(".css-q7yb35:nth-child(1) .css-11bijt4") %>% 
-   #   html_text() %>% .[1]
-  
-  
-  rating = 
-    data %>% 
-    str_split(name) %>%
-    .[[1]] %>% .[2] %>% 
-    str_split(review_num, n = 2) %>% 
-    .[[1]] %>% .[1] 
-  
+    data = 
+      html_page %>% 
+      html_elements(".list__09f24__ynIEd") %>% 
+      html_text() %>% .[1]
     
-  tibble(
-    name = name,
-    distance = distance,
-    rating = rating, 
-    review_num = review_num,
-    price = price,
-    data = data
+    info = 
+      html_page %>% 
+      html_elements(".css-chan6m") %>% 
+      html_text() 
     
-  )
+    review_num =
+      info[2]
+    
+    distance = 
+      info[4] %>% 
+      str_split("Miles", n=2) %>% 
+      .[[1]] %>% .[1] %>% 
+      as.double()
+    
+    name = 
+      html_page %>%  
+      html_elements(".css-1egxyvc") %>% 
+      html_text() %>% .[1] %>% 
+      str_sub(4)
+    
+    price =
+      html_page %>% 
+      html_elements(".css-1s7bx9e") %>%
+      html_text() %>% .[1]
+    
+    # type =
+    #   html_page %>%
+    #   html_elements(".css-q7yb35:nth-child(1) .css-11bijt4") %>%
+    #   html_text() %>% .[1]
+    
+    # rating = 
+    #   data %>% 
+    #   str_split(name) %>%
+    #   .[[1]] %>% .[2] %>% 
+    #   str_split(review_num, n = 2) %>% 
+    #   .[[1]] %>% .[1] 
+    
+    rating = 
+      html_page %>% 
+      html_elements(".css-gutk1c") %>%
+      html_text() %>% .[4]
+    
+    closeAllConnections()
+    
+    return(tibble(
+      name = name,
+      distance = distance,
+      rating = as.double(rating), 
+      review_num = review_num,
+      price = price,
+      data = ifelse(is.na(name), NA, data)
+    ))
+  }, 
+  error=function(e){
+    message(paste("URL does not exist:", url, "\n"))
+    return(
+      tibble(
+        name = NA,
+        distance = NA,
+        rating = NA, 
+        review_num = NA,
+        price = NA,
+        data = NA
+      )
+    )
+  })
+  
 }
 
+start_input = readline(prompt="Enter start index: ")
+start_index = as.integer(start_input)
+end_input = readline(prompt="Enter end index: ")
+end_index = as.integer(end_input)
+
+start_time = Sys.time()
+
+
 # get info
-res_data_3 =
-  res_data %>% slice(1000:1020) %>% 
-  mutate(yelp = map(.x = url, ~ get_info(.x))) %>% 
-  unnest(yelp)
+res_data_1 =
+  res_data %>% slice(start_index:end_index) %>% 
+  mutate(
+    yelp = map(.x = url, ~ get_info(.x))
+   ) %>% 
+  unnest(yelp) 
+
+end_time1 = Sys.time()
+running_time_1 = end_time1 - start_time
+
+res_data_2 = 
+  res_data_1 %>% 
+  drop_na(name, data) %>% 
+  filter(
+    is.na(rating) | is.na(distance)
+  ) %>% 
+  select(rowid:url) %>% 
+  mutate(
+    yelp = map(.x = url, ~ get_info(.x))
+  ) %>% 
+  unnest(yelp) 
+
+end_time2 = Sys.time()
+running_time_2 = end_time2 - start_time
+
+res_data_3 = 
+  res_data_2 %>% 
+  drop_na(name, data) %>% 
+  filter(
+    is.na(rating) | is.na(distance)
+  ) %>% 
+  select(rowid:url) %>% 
+  mutate(
+    yelp = map(.x = url, ~ get_info(.x))
+  ) %>% 
+  unnest(yelp) 
+
+end_time3 = Sys.time()
+running_time_3 = end_time3 - start_time
+
+res_data_4 = 
+  res_data_3 %>% 
+  drop_na(name, data) %>% 
+  filter(
+    is.na(rating) | is.na(distance)
+  ) %>% 
+  select(rowid:url) %>% 
+  mutate(
+    yelp = map(.x = url, ~ get_info(.x))
+  ) %>% 
+  unnest(yelp) 
+
+end_time4 = Sys.time()
+running_time_4 = end_time4 - start_time
+
+res_data_5 = 
+  res_data_4 %>% 
+  drop_na(name, data) %>% 
+  filter(
+    is.na(rating) | is.na(distance)
+  ) %>% 
+  select(rowid:url) %>% 
+  mutate(
+    yelp = map(.x = url, ~ get_info(.x))
+  ) %>% 
+  unnest(yelp) 
+
+end_time5 = Sys.time()
+running_time_5 = end_time5 - start_time
+
+result = res_data_1 %>% 
+  rbind(res_data_2) %>%
+  rbind(res_data_3) %>%
+  rbind(res_data_4) %>%
+  drop_na(distance) %>% 
+  rbind(res_data_5) %>% 
+  drop_na(rating) %>% 
+  mutate(
+    review_num = str_split(
+      review_num, "review", n = 2, simplify = TRUE) %>%
+      .[,1] %>% 
+      str_sub(2, -2) %>% 
+      as.integer()
+  )
   
+# end_time = Sys.time()
+# running_time = end_time - start_time
+
+c(running_time_1, running_time_2, running_time_3, running_time_4, running_time_5)
+
+
+## timing history (asyn)
+# 51 samples / run 2 times => 2.58 min => 35 ratings (32 valid)
+# 51 samples / run 3 times => 3.09 min => 40 ratings (39 valid)
+# 51 samples / run 4 times => 3.45 min => 42 ratings (41 valid)
+
+## timing in one run
+## 51 samples => all results / valid results
+# 2 iteration => 2.77 min => 23+6+3 = 32/29 results
+# 3 iteration => 3.42 min => 23+6+9+3 = 41/39 results
+# 4 iteration => 3.77 min => 23+6+9+2+3 = 43/40 results
+# 5 iteration => 4.05 min => 23+6+9+2+3+0 = 43/43 results
+
 # save
+
+file_name = str_c("./data/yelp/yelp_data_", as.character(start_index), "_", as.character(end_index), ".csv")
+
+result %>% 
+  select(-data) %>% 
+  write_excel_csv(file = file_name)
+
+
