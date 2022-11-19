0274a2f203982405564d20a0fcef3ecc9a9fc4c5
Tianshu Liu <tl3196@columbia.edu>
2022-11-19 00:57:06 -0500

    merge yelp data

---
+++ data_pre/yelp_merge.R
@@ -0,0 +1,32 @@
+# merge review data
+
+library(tidyverse)
+
+file_path = "./data/yelp/"
+file_list = list.files("./data/yelp/")
+
+yelp_df = tibble()
+
+for (file in file_list){
+  yelp_sub_df = read_csv(str_c(file_path, file))
+  yelp_df = yelp_df %>% rbind(yelp_sub_df)
+}
+
+yelp_df = 
+  yelp_df %>% 
+  mutate(
+    rowid = as.integer(rowid)
+  ) %>% 
+  arrange(rowid)
+
+# check duplicate
+
+yelp_df %>% 
+  count(rowid) %>% 
+  arrange(desc(n))
+
+# export
+
+# yelp_df %>% 
+#   write_excel_csv("./data/yelp_data.csv")
+
