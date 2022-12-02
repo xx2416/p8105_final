b7150e88fbe95116c62f5abbbf0907a02077fc43
Tianshu Liu <tl3196@columbia.edu>
2022-12-02 15:21:51 -0500

    real tidy merged inspection & yelp data

--- data_pre/yelp_merge.R
+++ data_pre/yelp_merge.R
@@ -17,7 +17,9 @@ yelp_df =
   mutate(
     rowid = as.integer(rowid)
   ) %>% 
-  arrange(rowid)
+  drop_na(distance) %>% 
+  filter(distance < 1.5) %>% 
+  arrange(rowid) 
 
 # check duplicate
 
@@ -30,5 +32,27 @@ yelp_df %>%
 # yelp_df %>% write_excel_csv("./data/yelp_data.csv")
 
 inspection_df = 
-  read_csv("./data/inspection_data.csv")
+  read_csv("./data/inspection_data.csv") %>% 
+  distinct() %>% 
+  select(-location_point)
 
+inspection_all_date = 
+  inspection_df %>% 
+  right_join(yelp_df %>% select(-rowid), keep = FALSE)
+
+inspection_all_date %>% write_excel_csv("./data/inspection_sub_all_date.csv")
+
+
+inspection_sig_date = 
+  inspection_all_date %>% 
+  nest(inspection_detail = action:critical_flag)
+
+inspection_sig_date %>% write_excel_csv("./data/inspection_sub_sig_date.csv")
+
+inspection_latest_date = 
+  inspection_sig_date %>% 
+  group_by(camis) %>% 
+  slice(which.max(as.Date(inspection_date))) 
+
+inspection_latest_date %>% write_excel_csv("./data/inspection_sub_latest_date.csv")
+  
